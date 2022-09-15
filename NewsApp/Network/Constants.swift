014405820fce2ae2d0880907ef3a2ea24a15bb8e
edabarutcu <edabarutcu@protonmail.com>
2022-09-15 14:26:26 +0300

    -baseURL and apiKey added

--- NewsApp/Network/Constants.swift
+++ NewsApp/Network/Constants.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+struct Constants {
+    struct ProductionServer {
+        static let baseURL = "http://newsapi.org/v2/top-headlines?country=tr&"
+
+    }
+    
+    struct APIParameterKey {
+        static let countryCode = "country_code"
+        static let randomCode = "random_code"
+        static let cPassword = "c_password"
+        static let apiKey = "d4b26469d3af47b19e0bc5228465fc0e"
+    }
+}
+
+enum HTTPHeaderField: String {
+    case authentication = "Authorization"
+    case contentType = "Content-Type"
+    case acceptType = "Accept"
+    case acceptEncoding = "Accept-Encoding"
+}
+
+enum ContentType: String {
+    case json = "application/json"
+}
